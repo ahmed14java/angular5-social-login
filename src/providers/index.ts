af82bbbb1f20b7002d467168ee0817ed508d2cf7
ahmed14java <ahmed14java@gmail.com>
2018-04-23 14:46:01 +0200

    Add Twitter Support

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -2,3 +2,4 @@
 export { GoogleLoginProvider } from './google-login-provider';
 export { FacebookLoginProvider } from './facebook-login-provider';
 export { LinkedinLoginProvider } from './linkedin-login-provider';
+export { TwitterLoginProvider } from './twitter-login-provider';
